9dac88b8d59f90cb525002d64ace64c5fc7fe344
Ester Martínez <ester.frontend@gmail.com>
2024-02-25 22:46:58 +0100

    Estilos generales

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import ProductPage from './Components/ProductPage/ProductPage'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
   return <>
+    <Header />
     <ProductPage />
+    <Footer />
   </>
 }
 
