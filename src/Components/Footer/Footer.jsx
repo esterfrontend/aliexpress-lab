18f96c63679f3373c48bb0ecd8530db0c6a80c74
Ester Martínez <ester.frontend@gmail.com>
2024-02-25 22:46:13 +0100

    Header y Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+import InstagramIcon from '../InstagramIcon/InstagramIcon'
+import TiktokIcon from '../TiktokIcon/TiktokIcon'
+import FacebookIcon from '../FacebookIcon/FacebookIcon'
+
+const Footer = () => {
+    return <div className='footer container'>
+        <InstagramIcon />
+        <TiktokIcon />
+        <FacebookIcon />
+    </div>
+}
+
+export default Footer
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+import Logo from '../Logo/Logo'
+
+const Header = () => {
+    return <div className='header container'>
+        <Logo />
+    </div>
+}
+
+export default Header
